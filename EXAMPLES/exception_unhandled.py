0600e5e627c8a0c21ba5782000987c855a3784de
jstrickler <jstrickler@gmail.com>
2025-09-10 11:49:24 -0400

    added new example for try-finally

--- EXAMPLES/exception_unhandled.py
+++ EXAMPLES/exception_unhandled.py
@@ -1,4 +1,17 @@
 x = 5
 y = "cheese"
 
-z = x + y  # Adding a string to an int raises TypeError
+def my_silly_add(a, b):
+    result = a + b
+    return result
+
+try:
+    result = my_silly_add(x, y)
+except (TypeError, ValueError) as err:
+    print(err)  # log the error???
+except Exception as err:
+    print("UNEXPECTED:", err)
+else: # no errors
+    print(f"{result = }")
+
+print("some more code here...")
